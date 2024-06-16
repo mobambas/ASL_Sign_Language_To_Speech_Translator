1826cb4ae38de653d81399ebb00343eebfce467d
Shriyansh Singh <99650602+mobambas@users.noreply.github.com>
2024-06-16 13:00:45 +0800

    Update data_collection_final.py

--- CUSTOM_OBJECT_DETECTION_MODEL/data_collection_final.py
+++ CUSTOM_OBJECT_DETECTION_MODEL/data_collection_final.py
@@ -5,8 +5,8 @@ import os as oss
 import traceback
 
 capture = cv2.VideoCapture(0)
-hd = HandDetector(maxHands=1)
-hd2 = HandDetector(maxHands=1)
+hd = HandDetector(maxHands=1, detectionCon=0.8)  # Increased confidence threshold
+hd2 = HandDetector(maxHands=1, detectionCon=0.8)  # Increased confidence threshold
 
 count = len(oss.listdir("C:\\Users\\Arush\\ASL_Sign_Language_To_Speech_Translator\\CUSTOM_OBJECT_DETECTION_MODEL\\AtoZ_3.1\\A\\"))
 c_dir = 'A'
@@ -15,6 +15,7 @@ offset = 15
 step = 1
 flag = False
 suv = 0
+max_images = 80  # Set a limit for the number of images to capture
 
 white = np.ones((400, 400), np.uint8) * 255
 cv2.imwrite("C:\\Users\\Arush\\ASL_Sign_Language_To_Speech_Translator\\CUSTOM_OBJECT_DETECTION_MODEL\\white.jpg", white)
@@ -23,65 +24,58 @@ while True:
     try:
         _, frame = capture.read()
         frame = cv2.flip(frame, 1)
-        hands = hd.findHands(frame, draw=False, flipType=True)
+        hands, frame = hd.findHands(frame, draw=False, flipType=True)
+
+        print("Hands detected:", hands)  # Debug print to check hands structure
+
         white = cv2.imread("C:\\Users\\Arush\\ASL_Sign_Language_To_Speech_Translator\\CUSTOM_OBJECT_DETECTION_MODEL\\white.jpg")
 
         if hands:
             hand = hands[0]
-            x, y, w, h = hand['bbox']
-            image = np.array(frame[y - offset:y + h + offset, x - offset:x + w + offset])
+            if isinstance(hand, dict) and 'bbox' in hand:
+                x, y, w, h = hand['bbox']
+                image = np.array(frame[y - offset:y + h + offset, x - offset:x + w + offset])
 
-            handz, imz = hd2.findHands(image, draw=True, flipType=True)
-            if handz:
-                hand = handz[0]
-                pts = hand['lmList']
-                # x1,y1,w1,h1=hand['bbox']
-                os = ((400 - w) // 2) - 15
-                os1 = ((400 - h) // 2) - 15
+                handz, imz = hd2.findHands(image, draw=True, flipType=True)
+                if handz:
+                    hand = handz[0]
+                    pts = hand['lmList']
+                    os = ((400 - w) // 2) - 15
+                    os1 = ((400 - h) // 2) - 15
 
-                # Drawing lines for the skeleton
-                for t in range(0, 4, 1):
-                    cv2.line(white, (pts[t][0] + os, pts[t][1] + os1), (pts[t + 1][0] + os, pts[t + 1][1] + os1),
-                             (0, 255, 0), 3)
-                for t in range(5, 8, 1):
-                    cv2.line(white, (pts[t][0] + os, pts[t][1] + os1), (pts[t + 1][0] + os, pts[t + 1][1] + os1),
-                             (0, 255, 0), 3)
-                for t in range(9, 12, 1):
-                    cv2.line(white, (pts[t][0] + os, pts[t][1] + os1), (pts[t + 1][0] + os, pts[t + 1][1] + os1),
-                             (0, 255, 0), 3)
-                for t in range(13, 16, 1):
-                    cv2.line(white, (pts[t][0] + os, pts[t][1] + os1), (pts[t + 1][0] + os, pts[t + 1][1] + os1),
-                             (0, 255, 0), 3)
-                for t in range(17, 20, 1):
-                    cv2.line(white, (pts[t][0] + os, pts[t][1] + os1), (pts[t + 1][0] + os, pts[t + 1][1] + os1),
-                             (0, 255, 0), 3)
-                cv2.line(white, (pts[5][0] + os, pts[5][1] + os1), (pts[9][0] + os, pts[9][1] + os1),
-                         (0, 255, 0), 3)
-                cv2.line(white, (pts[9][0] + os, pts[9][1] + os1), (pts[13][0] + os, pts[13][1] + os1),
-                         (0, 255, 0), 3)
-                cv2.line(white, (pts[13][0] + os, pts[13][1] + os1), (pts[17][0] + os, pts[17][1] + os1),
-                         (0, 255, 0), 3)
-                cv2.line(white, (pts[0][0] + os, pts[0][1] + os1), (pts[5][0] + os, pts[5][1] + os1),
-                         (0, 255, 0), 3)
-                cv2.line(white, (pts[0][0] + os, pts[0][1] + os1), (pts[17][0] + os, pts[17][1] + os1),
-                         (0, 255, 0), 3)
+                    for t in range(0, 4, 1):
+                        cv2.line(white, (pts[t][0] + os, pts[t][1] + os1), (pts[t + 1][0] + os, pts[t + 1][1] + os1), (0, 255, 0), 3)
+                    for t in range(5, 8, 1):
+                        cv2.line(white, (pts[t][0] + os, pts[t][1] + os1), (pts[t + 1][0] + os, pts[t + 1][1] + os1), (0, 255, 0), 3)
+                    for t in range(9, 12, 1):
+                        cv2.line(white, (pts[t][0] + os, pts[t][1] + os1), (pts[t + 1][0] + os, pts[t + 1][1] + os1), (0, 255, 0), 3)
+                    for t in range(13, 16, 1):
+                        cv2.line(white, (pts[t][0] + os, pts[t][1] + os1), (pts[t + 1][0] + os, pts[t + 1][1] + os1), (0, 255, 0), 3)
+                    for t in range(17, 20, 1):
+                        cv2.line(white, (pts[t][0] + os, pts[t][1] + os1), (pts[t + 1][0] + os, pts[t + 1][1] + os1), (0, 255, 0), 3)
+                    cv2.line(white, (pts[5][0] + os, pts[5][1] + os1), (pts[9][0] + os, pts[9][1] + os1), (0, 255, 0), 3)
+                    cv2.line(white, (pts[9][0] + os, pts[9][1] + os1), (pts[13][0] + os, pts[13][1] + os1), (0, 255, 0), 3)
+                    cv2.line(white, (pts[13][0] + os, pts[13][1] + os1), (pts[17][0] + os, pts[17][1] + os1), (0, 255, 0), 3)
+                    cv2.line(white, (pts[0][0] + os, pts[0][1] + os1), (pts[5][0] + os, pts[5][1] + os1), (0, 255, 0), 3)
+                    cv2.line(white, (pts[0][0] + os, pts[0][1] + os1), (pts[17][0] + os, pts[17][1] + os1), (0, 255, 0), 3)
 
-                skeleton0 = np.array(white)
-                zz = np.array(white)
-                for i in range(21):
-                    cv2.circle(white, (pts[i][0] + os, pts[i][1] + os1), 2, (0, 0, 255), 1)
+                    skeleton0 = np.array(white)
+                    zz = np.array(white)
+                    for i in range(21):
+                        cv2.circle(white, (pts[i][0] + os, pts[i][1] + os1), 2, (0, 0, 255), 1)
 
-                skeleton1 = np.array(white)
+                    skeleton1 = np.array(white)
 
-                cv2.imshow("1", skeleton1)
+                    cv2.imshow("1", skeleton1)
+            else:
+                print("Unexpected hand structure or 'bbox' key missing")
+        else:
+            print("No hands detected")
 
-        frame = cv2.putText(frame, "dir=" + str(c_dir) + "  count=" + str(count), (50, 50),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            1, (255, 0, 0), 1, cv2.LINE_AA)
+        frame = cv2.putText(frame, "dir=" + str(c_dir) + "  count=" + str(count), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
         cv2.imshow("frame", frame)
         interrupt = cv2.waitKey(1)
-        if interrupt & 0xFF == 27:
-            # esc key
+        if interrupt & 0xFF == 27:  # Press 'ESC' to exit
             break
 
         if interrupt & 0xFF == ord('n'):
@@ -91,7 +85,7 @@ while True:
             flag = False
             count = len(oss.listdir("C:\\Users\\Arush\\ASL_Sign_Language_To_Speech_Translator\\CUSTOM_OBJECT_DETECTION_MODEL\\AtoZ_3.1\\" + (c_dir) + "\\"))
 
-        if interrupt & 0xFF == ord('a'):
+        if interrupt & 0xFF == ord('a'):  # Press 'a' to start/stop capturing
             if flag:
                 flag = False
             else:
@@ -99,12 +93,12 @@ while True:
                 flag = True
 
         print("=====", flag)
-        if flag == True:
-            if suv == 180:
+        if flag:
+            if suv >= max_images:
                 flag = False
+                print("Max image limit reached")
             if step % 3 == 0:
-                cv2.imwrite("C:\\Users\\Arush\\ASL_Sign_Language_To_Speech_Translator\\CUSTOM_OBJECT_DETECTION_MODEL\\AtoZ_3.1\\" + (c_dir) + "\\" + str(count) + ".jpg",
-                            skeleton1)
+                cv2.imwrite("C:\\Users\\Arush\\ASL_Sign_Language_To_Speech_Translator\\CUSTOM_OBJECT_DETECTION_MODEL\\AtoZ_3.1\\" + (c_dir) + "\\" + str(count) + ".jpg", skeleton1)
                 count += 1
                 suv += 1
             step += 1
